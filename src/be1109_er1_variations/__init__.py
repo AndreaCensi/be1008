8a706b61e45682d95a872971d713b1185a31196b
Andrea Censi <andrea@cds.caltech.edu>
2011-09-05 10:50:58 -0700

    fixed

--- src/be1109_er1_variations/__init__.py
+++ src/be1109_er1_variations/__init__.py
@@ -1,4 +1,4 @@
 from procgraph import pg_add_this_package_models
-pg_add_this_package_models(file=__file__, assign_to=__package__)
+pg_add_this_package_models(__file__, assign_to=__package__)
 
 from .discretize_command import *
